34a4b26e0d19fa5c01c83b147e5c28b837a38784
cang8 <0.gizemgunes@gmail.com>
2023-11-22 23:30:48 +0300

    products-controller-add-select

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -23,7 +23,15 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok(_productReadRepository.GetAll(false));
+            return Ok(_productReadRepository.GetAll(false).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdateDate
+            }));
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
@@ -33,10 +41,6 @@ namespace ETicaretAPI.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
-            if (ModelState.IsValid) 
-            {
-                
-            }
             await _productWriteRepository.AddAsync(new()
             {
                 Name = model.Name,
