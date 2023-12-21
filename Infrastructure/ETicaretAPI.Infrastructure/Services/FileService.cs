302ada15cc4774773aa1f16f5224c2c952a7ff21
cang8 <0.gizemgunes@gmail.com>
2023-12-21 19:26:45 +0300

    edit-filerenameasync

--- Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
@@ -45,12 +45,26 @@ namespace ETicaretAPI.Infrastructure.Services
                         newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
                     else
                     {
+                        int lastIndex = 0;
+                        while (true)
+                        {
+                            lastIndex = indexNo1;
+                            indexNo1 = newFileName.IndexOf("-", indexNo1 + 1);
+                            if (indexNo1 == -1)
+                            {
+                                indexNo1 = lastIndex;
+                                break;
+                            }
+                        }
                         int indexNo2 = newFileName.IndexOf(".");
-                        string fileNo = newFileName.Substring(indexNo1, indexNo2 - indexNo1 - 1);
-                        int _fileNo = int.Parse(fileNo);
-                        _fileNo++;
+                        string fileNo = newFileName.Substring(indexNo1 + 1, indexNo2 - indexNo1 - 1);
 
-                        newFileName = newFileName.Remove(indexNo1, indexNo2 - indexNo1 - 1).Insert(indexNo1, _fileNo.ToString());
+                        if (int.TryParse(fileNo, out int _fileNo))
+                        {
+                            _fileNo++;
+                            newFileName = newFileName.Remove(indexNo1 + 1, indexNo2 - indexNo1 - 1).Insert(indexNo1 + 1, _fileNo.ToString());
+                        }else
+                            newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
                     }
                 }
 
