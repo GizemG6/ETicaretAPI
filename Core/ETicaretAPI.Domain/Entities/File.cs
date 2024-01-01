2f1b780d2b3c609c86a5919aed0d67e27cf0924d
cang8 <0.gizemgunes@gmail.com>
2024-01-01 22:53:01 +0300

    add-file-in-entities-and-mig4

---
+++ Core/ETicaretAPI.Domain/Entities/File.cs
@@ -0,0 +1,16 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class File : BaseEntity
+    {
+        [NotMapped]
+        public override DateTime UpdateDate { get => base.UpdateDate; set => base.UpdateDate = value; }
+    }
+}
