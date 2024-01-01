041cd45a4044906676612a809be68a49f7d9bdea
cang8 <0.gizemgunes@gmail.com>
2024-01-01 23:52:07 +0300

    add-file-read-write-repository

--- Core/ETicaretAPI.Domain/Entities/File.cs
+++ Core/ETicaretAPI.Domain/Entities/File.cs
@@ -10,6 +10,8 @@ namespace ETicaretAPI.Domain.Entities
 {
     public class File : BaseEntity
     {
+        public string FileName { get; set; }
+        public string Path { get; set; }
         [NotMapped]
         public override DateTime UpdateDate { get => base.UpdateDate; set => base.UpdateDate = value; }
     }
--- Core/ETicaretAPI.Domain/Entities/InvoiceFile.cs
+++ Core/ETicaretAPI.Domain/Entities/InvoiceFile.cs
@@ -8,5 +8,6 @@ namespace ETicaretAPI.Domain.Entities
 {
     public class InvoiceFile : File
     {
+        public decimal Price { get; set; }
     }
 }
